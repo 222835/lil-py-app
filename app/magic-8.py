9e8df43d531e4c959c5164e3f5db45c960afa6ae
Lizbeth.B <al222835@alumnos.uacj.mx>
2025-03-03 11:36:51 -0700

    added a little pyhton app that simulates a magic 8-ball

---
+++ app/magic-8.py
@@ -0,0 +1,32 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.magic8ball = ["Yes, definetly", "No!!", "Perchance...", "Ask me later", "idk", "OMG YES", "mmmm i would think abt it"]
+
+        self.button = QtWidgets.QPushButton("New answer")
+        self.text = QtWidgets.QLabel("Ask and you shall get an answer...",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.magic8ball))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
